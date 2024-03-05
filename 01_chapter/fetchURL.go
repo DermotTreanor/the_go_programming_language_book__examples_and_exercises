7045b31742666bf4b19746f2a4fef4e1fd5a83da
Dermot Treanor <dermot.treanor1@gmail.com>
2024-03-05 07:49:40 +0000

    Updated the fetch program in chapter 1 so that it closes the reader and prints the output to the screen (it's no longer getting an image)

--- 01_chapter/fetchURL.go
+++ 01_chapter/fetchURL.go
@@ -8,17 +8,17 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://styles.redditmedia.com/t5_2rc7j/styles/communityIcon_wy4riduoe9k11.png") //http://127.0.0.1:8000/support_ireland")
+	resp, err := http.Get("http://127.0.0.1:8000/support_ireland")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "fetch error:%v\n", err)
 		os.Exit(1)
 	}
 	// fmt.Print(resp)
 	byteSlice, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
 	}
-	fmt.Print(byteSlice)
-	file, err := os.OpenFile("image", os.O_WRONLY|os.O_CREATE, 0666)
-	file.Write(byteSlice)
+	fmt.Printf("%s\n", byteSlice)
+
 }
