0a97cc2809ec0b2ae71b23b29e3c3357ce8bf2b0
Dermot Treanor <dermot.treanor1@gmail.com>
2024-05-11 11:14:32 +0100

    fixed error by adding flag parse function

--- 08_chapter/multi_client/clockwall.go
+++ 08_chapter/multi_client/clockwall.go
@@ -11,10 +11,11 @@ import (
 var port *int = flag.Int("p", 8000, "specifies the port to dial")
 
 func main() {
+	flag.Parse()
 	address := fmt.Sprintf("localhost:%d", *port)
 
 	//Connect to a server
 	connection, _ := net.Dial("tcp", address)
-
+	//Read from the connection and write to StdOut
 	io.Copy(os.Stdout, connection)
 }
