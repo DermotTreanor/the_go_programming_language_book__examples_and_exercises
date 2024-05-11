58387801014e975e2c148da7ca6baecde4e91e04
Dermot Treanor <dermot.treanor1@gmail.com>
2024-05-11 09:10:09 +0100

    error checking listener creation and flags that don't work yet

--- 08_chapter/server/server_clock.go
+++ 08_chapter/server/server_clock.go
@@ -1,8 +1,10 @@
 package main
 
-import(
+import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
 	"time"
 )
 
@@ -10,25 +12,35 @@ func handleConnection(c net.Conn){
 	defer c.Close()
 	fmt.Fprintln(c, "Hello", "there!")
 	for{
-		_, err := fmt.Fprintln(c, time.Now())
+		_, err := fmt.Fprint(c, time.Now().Format("15:04:05\r"))
 		if err != nil{
 			fmt.Println(err)
 			return 
 		}
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
 
-func main(){
-	
-	//Create a listener
-	listener, _ := net.Listen("tcp", "localhost:8000")
 
+var port int = *flag.Int("port", 8000, "selects the port number to bind the server to")
+
+func main(){
+
+	flag.Parse()
+	address := fmt.Sprintf("localhost:%d", port)
+	fmt.Println(port)
+	//Create a listener
+	listener, err := net.Listen("tcp", address)
+	if err !=nil{
+		fmt.Fprintf(os.Stderr, "Error while making listener:%T\n%v\n\nClosing server program...\n", err, err)
+		return
+	}
+	
 	for{
 		//Block until we hear a connection
 		connection, _ := listener.Accept()
 		//Pass the net.Conn object to a function to handle
-		go handleConnection(connection)
+		handleConnection(connection)
 	}
 	
 }
